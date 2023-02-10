95c429627615c38c8b7eeae9ecaea38852e6db60
Vladimir Kalinin <Vofanchic@mail.ru>
2023-02-10 20:29:20 +0500

    main ui created

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#https://online-json.com/json-editor
 from pprint import pprint
 
 import requests
 
 def api_request(customerregion=66, perpage=1, productsearch=None):
-    payloads_ = {f'customerregion': customerregion, 'perpage': perpage, 'currentstage': 'EC', 'sort': '-signDate'}
+    payloads_ = {f'customerregion': customerregion, 'perpage': perpage,
+                 'currentstage': 'EC', 'sort': '-signDate', 'fz': 44}
     response = requests.get(f"http://openapi.clearspending.ru/restapi/v3/contracts/search/?productsearch={productsearch}",
                             params=payloads_)
     l = list(response.json()['contracts']['data'])
